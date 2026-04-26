73faa680aae009db42fb7e580b8cd56ea410247b
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2026-04-26 15:52:08 -0300

    feat: adiciona implementação da estrutura de dados Fila (Queue) em C#

---
+++ csharp/basics/Queues/Queues/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Queue<int> queue = new Queue<int>();
+    
+        // 1. ENQUEUE: Adiciona ao final da fila
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+        
+        int result = queue.Dequeue();
+        
+        // 2. PEEK: Olha quem está na frente (sem remover)
+        Console.WriteLine(result);
+        
+        // 3. DEQUEUE: Remove e retorna quem está na frente
+        int remove = queue.Dequeue();
+        
+
+    }
+}
---
+++ csharp/basics/Queues/README.md
@@ -0,0 +1,7 @@
+# 📚 Queues em C# (`Queue<T>`) - A Base do BFS
+
+A Fila (Queue) é uma estrutura de dados FIFO (Primeiro a Entrar, Primeiro a Sair). É a estrutura ideal para algoritmos que precisam explorar dados em "camadas" ou níveis, como a Busca em Largura (BFS).
+
+## 1. Declaração e Inicialização
+```csharp
+Queue<int> fila = new Queue<int>();
